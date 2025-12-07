7a23d3715d96f7bae194977ab6e0e53c673bae84
Rubelito Abella <bbadoodles@gmail.com>
2025-12-07 14:14:22 +0800

    add day7 code

---
+++ day7/tachyonPropagation.kt
@@ -0,0 +1,43 @@
+package day7
+
+import java.io.File
+
+fun List<Long>.propagate(nextLine: String): Pair<List<Long>, Long> {
+    val nextLineChars = nextLine.toCharArray()
+    val propagation: MutableList<Long> = MutableList(this.size) { 0L }
+    var splits = 0L
+    for ((i, char) in nextLine.withIndex()) {
+        if (this[i] > 0 && char == '^') {
+            propagation[i - 1] += this[i]
+            propagation[i + 1] += this[i]
+            splits += 1
+        } else if (this[i] > 0) {
+            propagation[i] += this[i]
+        }
+    }
+    return propagation to splits
+}
+
+fun main() {
+    val lines = File("day7/input.in").readLines()
+    println()
+    var propagation = lines.first().map {
+        when (it) {
+            'S' -> 1L
+            else -> 0L
+        }
+    }
+    var totalSplits = 0L
+    lines.drop(1).forEach {
+        println(propagation)
+        propagation.propagate(it).let { (newLine, splits) ->
+            propagation = newLine
+            totalSplits += splits
+        }
+    }
+
+    println(propagation)
+    println(propagation.sum())
+    println(totalSplits)
+
+}
